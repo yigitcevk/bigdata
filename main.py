47785e706651c693bbab9cd62aa1fb0d616a0cbd
Onur1258 <onur.deniz_1999@hotmail.com>
2023-11-23 22:46:16 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyspark
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master("local[1]").appName("NBAAAAA").getOrCreate()
+print(spark)
+df1 = spark.read.csv("/Users/onurdeniz/Desktop/archive/Players.csv")
+df2 = spark.read.csv("/Users/onurdeniz/Desktop/archive/Seasons_Stats.csv")
+df3 = spark.read.csv("/Users/onurdeniz/Desktop/archive/player_data.csv")
+
+df1.show()
+df2.show()
+df3.show()
